a06d4f543ad96ccac3455486a35908a9f77fceb6
flatypus [mac] <yhc3141@gmail.com>
2023-11-09 11:49:41 -0800

    chore: semver

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = '0.2.2'
+VERSION = '0.2.3'
 DESCRIPTION = 'Streamlining the process of multi-prompting LLMs with chains'
 
 setup(
