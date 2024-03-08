c30e3c85015e8ed40d5ab5e664fd8a0b68485f5c
flatypus [mac] <yhc3141@gmail.com>
2024-03-07 17:03:54 -0800

    release: 1.1.0

--- upload.py
+++ upload.py
@@ -7,6 +7,9 @@ load_dotenv()
 username = "__token__"
 password = os.environ.get("PYPI_TOKEN")
 
+if not password:
+    raise ValueError("PYPI_TOKEN is not set")
+
 subprocess.run(["python3", "-m", "build"])
 subprocess.run([
     "python3", "-m", "twine", "upload", "dist/*", "--username", username, "--password", password
