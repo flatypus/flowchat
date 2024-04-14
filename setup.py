c9c65ed90ca1ed5cbc215a0a959ba6c442b62359
flatypus [mac] <yhc3141@gmail.com>
2024-04-14 13:18:26 -0700

    chore: update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = '1.1.3'
+VERSION = '1.1.4'
 DESCRIPTION = 'Streamlining the process of multi-prompting LLMs with chains'
 
 setup(
