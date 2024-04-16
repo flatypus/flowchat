ca2abcffdd311d9b6c153156e1973efce3ea35d0
flatypus [mac] <yhc3141@gmail.com>
2024-04-15 19:23:47 -0700

    fix: count usage for streams and images

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
-        "openai", "Pillow", "tiktoken"
+        "openai", "Pillow", "tiktoken", "requests"
     ],
     setup_requires=['pytest-runner', 'flake8'],
     tests_require=['pytest'],
